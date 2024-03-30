d26dd9252c0322dd1bcf71ad89967cc2189a1281
FrankFourierX <f.f.tandoi@gmail.com>
2024-03-30 14:44:17 +0700

    first commit
    Repo forked from:
    
    https://github.com/leoMirandaa/shadcn-landing-page

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # interface
 🔶 Open source interfaces for the BondHive protocol
+
+## How to install
+
+1. Clone this repositoy:
+
+```bash
+git clone https://github.com/leoMirandaa/shadcn-landing-page.git
+```
+
+2. Go into project
+
+```bash
+cd shadcn-landing-page
+```
+
+3. Install dependencies
+
+```bash
+npm install
+```
+
+4. Run project
+
+```bash
+npm run dev
+```
---
+++ src/components/Hero.tsx
@@ -0,0 +1,28 @@
+export const Hero = () => {
+  return (
+    <section className="container place-items-center pt-20 md:pt-14 gap-10">
+      <div className="text-center lg:text-start space-y-6">
+        <main className="text-5xl md:text-6xl font-bold">
+          <h1 className="inline">
+            <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
+              Shadcn
+            </span>{" "}
+            landing page
+          </h1>{" "}
+          for{" "}
+          <h2 className="inline">
+            <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
+              React
+            </span>{" "}
+            developers
+          </h2>
+        </main>
+
+        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+          Build your React landing page effortlessly with the required sections
+          to your project.
+        </p>
+      </div>
+    </section>
+  );
+};
