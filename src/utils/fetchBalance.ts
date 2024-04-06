8c3a8bef37c0b328c193a53c6a77543a2c79627a
FrankFourierX <f.f.tandoi@gmail.com>
2024-04-07 06:16:35 +0700

    remove folder

--- src/utils/fetchBalance.ts
+++
@@ -1,7 +0,0 @@
-import { Connection, PublicKey } from '@solana/web3.js';
-
-export const fetchWalletBalance = async (connection: Connection, walletAddress: string): Promise<number> => {
-    const walletPublicKey = new PublicKey(walletAddress);
-    const balance = await connection.getBalance(walletPublicKey);
-    return balance / Math.pow(10, 9); // Convert lamports to SOL
-};
